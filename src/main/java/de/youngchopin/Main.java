bbd6aaa371d18ceafbb47715a6d98170ede6c687
YoungChopin <140334565+YoungChopin@users.noreply.github.com>
2023-09-09 20:25:44 +0200

    whooops

--- src/main/java/de/youngchopin/Main.java
+++ src/main/java/de/youngchopin/Main.java
@@ -9,7 +9,7 @@ public class Main{
         public static void main(String[] args) {
                 ConsoleGUI consoleGUI = new ConsoleGUI();
                 consoleGUI.setVisible(true);
-                String token = "MTE0OTQ0NDQ4MjY3MzE2NDM2OA.GVXi__.WiCLMHlBoNCw-1jOMepehTBJJNTKIKxkbJU2m0";
+                String token = TOKEN;
                 DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
                 builder.setEnabledIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.SCHEDULED_EVENTS, GatewayIntent.GUILD_MESSAGES);
                 builder.enableCache(CacheFlag.VOICE_STATE);
